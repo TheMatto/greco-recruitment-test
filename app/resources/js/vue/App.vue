82c74ce3ba3b60c7d914252243f1d058475bcec9
TheMatto <maticjan@gmail.com>
2024-10-23 12:48:52 +0200

    google autocomplete location

--- app/resources/js/vue/App.vue
+++ app/resources/js/vue/App.vue
@@ -1,20 +1,35 @@
 <template>
   <h1>Find nearest sites</h1>
   <form class="form" @submit.prevent="submit" novalidate>
-    <div class="input">
+    <div v-show="manualEntry" class="input">
       <label>Lat</label>
       <input v-model="lat" type="text">
     </div>
-    <div class="input">
+    <div v-show="manualEntry" class="input">
       <label>Lon</label>
       <input v-model="lon" type="text">
+    </div>
+    <div v-show="!manualEntry" class="input">
+      <label>Location</label>
+      <vue-google-autocomplete
+        id="autocomplete-location"
+        placeholder="Start typing"
+        @placechanged="onPlaceChanged"
+      />
+    </div>
+    <div class="input">
+      <label>
+        <input v-model="manualEntry" type="checkbox">
+        <span>Manual coordinates entry</span>
+      </label>
     </div>
     <div class="input">
       <button class="button">Show</button>
     </div>
   </form>
+
   <div v-if="loading" class="loading">Loading...</div>
-  <div class="closest-sites">
+  <div v-show="sites.length" class="closest-sites">
     <h2>Results:</h2>
     <div v-for="site in sites" :key="site.id" class="site">
       <div class="name">{{ site.site }}</div>
@@ -29,12 +44,22 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+import VueGoogleAutocomplete from 'vue-google-autocomplete';
 
 const loading = ref(false);
 const sites = ref([]);
 
+const manualEntry = ref(false);
+
 const lat = ref('');
 const lon = ref('');
+
+const onPlaceChanged = (addressData, placeResultData, id) => {
+  if (addressData.latitude && addressData.longitude) {
+    lat.value = addressData.latitude;
+    lon.value = addressData.longitude;
+  }
+}
 
 const submit = async (values) => {
   loading.value = true;
--- app/templates/home.php
+++ app/templates/home.php
@@ -8,7 +8,7 @@
     <meta name="description" content="Backend Engineer Recruitment Test">
     <meta name="author" content="Matic Jan">
 
-    <title>Naslov strani</title>
+    <title>Backend Engineer Recruitment Test</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -19,6 +19,9 @@
     <?php elseif (file_exists($manifestPath = 'dist/.vite/manifest.json')): ?>
         <?php $manifest = json_decode(file_get_contents($manifestPath), true); ?>
         <link rel="stylesheet" href="<?php echo 'dist/' . $manifest['app/resources/js/index.js']['css'][0]; ?>">
+    <?php endif; ?>
+    <?php if (isset($_ENV['GOOGLE_API_KEY'])): ?>
+        <script src="https://maps.googleapis.com/maps/api/js?key=<?php echo $_ENV['GOOGLE_API_KEY']; ?>&libraries=places"></script>
     <?php endif; ?>
 </head>
 <body>
