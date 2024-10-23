728e75e98c4271011e26adc9dd9a35261035d833
TheMatto <maticjan@gmail.com>
2024-10-23 13:39:39 +0200

    simple client side validation

--- app/resources/js/vue/App.vue
+++ app/resources/js/vue/App.vue
@@ -1,20 +1,29 @@
 <template>
   <h1>Find nearest sites</h1>
-  <form class="form" @submit.prevent="submit" novalidate>
-    <div v-show="manualEntry" class="input">
+  <form class="form" @submit.prevent="submit(values)" novalidate>
+    <div v-show="manualEntry" class="input" :class="{ error: errors.lat }">
       <label>Lat</label>
-      <input v-model="lat" type="text">
+      <input
+        v-model="values.lat"
+        type="text"
+        @focus="errors.lat = false"
+      >
     </div>
-    <div v-show="manualEntry" class="input">
+    <div v-show="manualEntry" class="input" :class="{ error: errors.lon }">
       <label>Lon</label>
-      <input v-model="lon" type="text">
+      <input
+        v-model="values.lon"
+        type="text"
+        @focus="errors.lon = false"
+      >
     </div>
-    <div v-show="!manualEntry" class="input">
+    <div v-show="!manualEntry" class="input" :class="{ error: errors.lat || errors.lon }">
       <label>Location</label>
       <vue-google-autocomplete
         id="autocomplete-location"
         placeholder="Start typing"
         @placechanged="onPlaceChanged"
+        @focus="errors.lat = errors.lon = false"
       />
     </div>
     <div class="input">
@@ -42,7 +51,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 import axios from 'axios';
 import VueGoogleAutocomplete from 'vue-google-autocomplete';
 
@@ -51,24 +60,48 @@ const sites = ref([]);
 
 const manualEntry = ref(false);
 
-const lat = ref('');
-const lon = ref('');
+const values = reactive({
+  lat: '',
+  lon: ''
+});
+
+const errors = reactive({
+  lat: false,
+  lon: false
+});
 
 const onPlaceChanged = (addressData, placeResultData, id) => {
   if (addressData.latitude && addressData.longitude) {
-    lat.value = addressData.latitude;
-    lon.value = addressData.longitude;
+    values.lat = addressData.latitude;
+    values.lon = addressData.longitude;
   }
 }
 
 const submit = async (values) => {
+
+  const floatRegex = /^-?\d+(\.\d+)?$/;
+
+  let error = false;
+
+  for (const [key, value] of Object.entries(values)) {
+    if (!floatRegex.test(value)) {
+      errors[key] = true;
+
+      error = true;
+    } else {
+      errors[key] = false;
+    }
+  }
+
+  if (error) return;
+
   loading.value = true;
 
   try {
     const response = await axios.get('/api/closest-sites', {
       params: {
-        lat: parseFloat(lat.value),
-        lon: parseFloat(lon.value)
+        lat: parseFloat(values.lat),
+        lon: parseFloat(values.lon)
       }
     });
 
